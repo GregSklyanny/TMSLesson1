6ab3541a8485b621c9cf72a36eb9c4c98693fbce
GregSklyanny <gregsklyanny@gmail.com>
2024-12-12 19:32:45 +0300

    Initial commit

---
+++ TMSLesson1Console/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Reflection;
+
+namespace TMSLesson1Console
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Assembly assembly = Assembly.LoadFrom("../../../../TMSLesson1Library/bin/Debug/net8.0/TMSLesson1Library.dll");
+            var type = assembly.GetType("TMSLesson1Library.StringAnalyzer");
+            object o = Activator.CreateInstance(type);
+
+            while (true)
+            {
+                Console.WriteLine("Введите целое число");
+                string input = Console.ReadLine();
+                var s = type.InvokeMember("OddOrEvenAnalyzer", BindingFlags.Default | BindingFlags.InvokeMethod, null, o, new object[] { input }, null);
+                Console.WriteLine($"Ответ: {s.ToString()}");
+            }
+
+
+
+
+        }
+    }
+}
---
+++ TMSLesson1Library/StringAnalyzer.cs
@@ -0,0 +1,16 @@
+﻿namespace TMSLesson1Library
+{
+    internal class StringAnalyzer
+    {
+        public string OddOrEvenAnalyzer(string str)
+        {
+            int number;
+            if (int.TryParse(str, out number))
+            {
+                return number % 2 == 0 ?  $"Число {number} четное" : $"Число {number} нечетное";
+            }
+
+            return "Это не целое число";
+        }
+    }
+}
